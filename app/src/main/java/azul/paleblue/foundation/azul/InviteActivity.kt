3b2f4cf70064195d19712d8641ac0318991fd7a6
David Young-chan Kay <davidykay@gmail.com>
2018-09-11 09:11:40 -0700

    Got gRPC hello world working using gRPC Hello World protobuf.

--- app/src/main/java/azul/paleblue/foundation/azul/InviteActivity.kt
+++ app/src/main/java/azul/paleblue/foundation/azul/InviteActivity.kt
@@ -2,20 +2,50 @@ package azul.paleblue.foundation.azul
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import azul.paleblue.foundation.azul.network.ApiClient
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
-class InviteActivity : Activity() {
+class InviteActivity : Activity(), AnkoLogger {
+
+    fun makeEditable(s: String): Editable {
+      val f = Editable.Factory()
+      return f.newEditable(s)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val apiClient = ApiClient()
+
         verticalLayout {
             padding = dip(30)
 
+            val host = editText {
+              hint = "Host"
+              text = makeEditable("192.168.1.95")
+            }
+            val port = editText {
+              hint = "Port"
+              text = makeEditable("50051")
+            }
+            val name = editText {
+              hint = "Message"
+              text = makeEditable("Azul Wallet")
+            }
+
             button("Invite Activity") {
                 onClick {
-                    toast("I sent an invitation!")
+                    toast("Sending Invite")
+                    doAsync {
+                        info("Sending invite")
+                        val response = apiClient.sendMessage(host.text.toString(), port.text.toString(), name.text.toString())
+                        info("Invite response received: $response")
+                        //thread(start = true) {
+                        //println("running from thread(): ${Thread.currentThread()}")
+                        toast(response)
+                    }
                 }
             }
         }
--- app/src/main/java/azul/paleblue/foundation/azul/network/ApiClient.kt
+++ app/src/main/java/azul/paleblue/foundation/azul/network/ApiClient.kt
@@ -1,7 +1,54 @@
 package azul.paleblue.foundation.azul.network
 
-class ApiClient {
-  // TODO: Integrate gRPC
+import android.text.TextUtils
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+//import foundation.paleblue.azul.proto.*
 
+import io.grpc.examples.helloworld.GreeterGrpc
+import io.grpc.examples.helloworld.HelloRequest
 
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.debug
+import java.io.PrintWriter
+import java.io.StringWriter
+
+class ApiClient : AnkoLogger {
+    // TODO: Integrate gRPC
+
+    private var channel: ManagedChannel? = null
+
+    init {
+
+    }
+
+    //val defaultHost = "10.40.255.252"
+    val defaultHost = "192.168.1.95"
+    val defaultPort = "50051"
+
+    fun sendMessage(host: String, portStr: String, message: String): String {
+    //fun sendMessage(message: String): String {
+
+        //val host = defaultHost
+        //val portStr = defaultPort
+
+        val port = if (TextUtils.isEmpty(portStr)) 0 else Integer.valueOf(portStr)
+        debug("Got port: $port")
+        try {
+            channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
+            val stub = GreeterGrpc.newBlockingStub(channel)
+            val request = HelloRequest.newBuilder().setName(message).build()
+            val reply = stub.sayHello(request)
+            debug("Got reply: $reply")
+            return reply.message
+        } catch (e: Exception) {
+            val sw = StringWriter()
+            val pw = PrintWriter(sw)
+            e.printStackTrace(pw)
+            pw.flush()
+            return String.format("Failed... : %n%s", sw)
+        }
+
+    }
 }
+
