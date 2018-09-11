1518329e7089d3318a132360252ae3e15cb1669c
David Young-chan Kay <davidykay@gmail.com>
2018-09-11 08:20:56 -0700

    Initial import

---
+++ app/src/main/java/azul/paleblue/foundation/azul/MainActivity.kt
@@ -0,0 +1,12 @@
+package azul.paleblue.foundation.azul
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
