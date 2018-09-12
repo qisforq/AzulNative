cd55fd8922e4a6a2bd595c50d0b3c98868bbfd77
David Young-chan Kay <davidykay@gmail.com>
2018-09-12 14:43:55 -0700

    Google Play now passes Intent to a receiver. We grab the inviteCode and store in in SharedPreferences.

---
+++ app/src/main/java/azul/paleblue/foundation/azul/invite/InstallReceiver.kt
@@ -0,0 +1,19 @@
+package azul.paleblue.foundation.azul.invite
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import org.jetbrains.anko.*
+
+class InstallReceiver: BroadcastReceiver(), AnkoLogger {
+  override fun onReceive(context: Context?, intent: Intent?) {
+    verbose("InstallReceiver got intent")
+    if (intent!!.action == "com.android.vending.INSTALL_REFERRER") {
+      val referrer = intent.extras.getString("referrer")
+      val inviteCode = intent.extras.getString("inviteCode")
+      info("InstallReceiver got install with referrer: $referrer and code: $inviteCode")
+      InviteCodeStorage(context!!).storeCode(inviteCode)
+    }
+  }
+
+}
