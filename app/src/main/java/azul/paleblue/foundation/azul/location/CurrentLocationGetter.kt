fe1b4981ee22d8005f50477b6f9885b98d3230f8
Quentin Vidal <qisforq@gmail.com>
2018-09-12 16:31:56 -0400

    Implemented CurrentLocationGetter

--- app/src/main/java/azul/paleblue/foundation/azul/location/CurrentLocationGetter.kt
+++ app/src/main/java/azul/paleblue/foundation/azul/location/CurrentLocationGetter.kt
@@ -1,11 +1,23 @@
 package azul.paleblue.foundation.azul.location
 
+import android.content.Context
 import android.location.Location
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.OnSuccessListener
 
-class CurrentLocationGetter{
-    fun getCurrentLocation(): Location? {
-        // TODO: Return Current Location
-        // see https://developer.android.com/training/location/retrieve-current
-        return null
+class CurrentLocationGetter(context: Context) {
+
+
+    private val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
+    fun getCurrentLocation(onSuccess: OnSuccessListener<Location?>) {
+//         TODO: Return Current Location
+//         see https://developer.android.com/training/location/retrieve-current
+        val task = fusedLocationClient.lastLocation
+        task.addOnSuccessListener(onSuccess)
+        task.run {  }
     }
 }
+
+
